85b9e06a952eeb21632c893326b30e9c9f7fd098
SeanZitzer1 <sean.m.zitzer83@outlook.com>
2023-10-23 17:35:25 +0000

    final commit to project 2

--- script.js
+++ script.js
@@ -4,10 +4,31 @@ function validateForm() {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
-
-    const forbiddenUsernames = ["Admin", "Harry", "Elizabeth", "Shana"];
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    const forbiddenUsernames = ["Admin", "Harry", "Potter", "HufflePuff"]; //These are some usernames just to test the function is all 
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+
+    if (firstName.length < 2 || firstName.length > 50) {
+        alert("First name should be between 2 and 50 characters.");
+        return false; // Checks if the first name is in the min and max criteria does the same below for the last name as well
+    }
+
+    if (lastName.length < 2 || lastName.length > 50) {
+        alert("Last name should be between 2 and 50 characters.");
+        return false;
+    }
+
+    if (!emailPattern.test(email)) {
+        alert('Please enter a valid email address.');
+        return false;     // Checks if  the email entered is a valid email address
+    }
+
+    if (forbiddenUsernames.includes(username)) {
+        alert('Username already taken.');
+        return false;     // This would check if the username was already taken but we don't have that ability here just yet
+    }
 
     if(!password || !rePassword || !username || !email || !phone) {
         alert('Please fill out all fields.');
@@ -19,19 +40,9 @@ function validateForm() {
         return false;     // Checks if the passwords entered by the user match
     }
 
-    if (!emailPattern.test(email)) {
-        alert('Please enter a valid email address.');
-        return false;     // Checks if  the email entered is a valid email address
-    }
-
     if (!phonePattern.test(phone)) {
         alert('Please enter a valid phone number in the format ###-###-####.');
         return false;     // Checks if the phone number entered is valid (formatting is also checked by the pattern attribute in HTML normally but I wanted it done here so the pop-up box for errors             matched)
-    }
-
-    if (forbiddenUsernames.includes(username)) {
-        alert('Username already taken.');
-        return false;     // This would check if the username was already taken but we don't have that ability here just yet
     }
 
     return true;
